aaa09712eb3636b872b356ac67b0e0a79156745e
Fikayo <fik4christ@yahoo.com>
2019-11-17 23:42:46 +0100

    Test running

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
@@ -10,3 +10,27 @@ module.exports = {
     '.*\\.(vue)$': 'vue-jest',
   }
 }
+ */
+
+module.exports = {
+  // tell Jest to handle `*.vue` files
+  moduleFileExtensions: ["js", "json", "vue"],
+  watchman: false,
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+    "^~~/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/$1"
+  },
+  transform: {
+    // process js with `babel-jest`
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    // process `*.vue` files with `vue-jest`
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  },
+  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/components/**/*.vue",
+    "<rootDir>/pages/*.vue"
+  ]
+};
