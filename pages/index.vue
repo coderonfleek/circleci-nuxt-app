6d0637858aed4983054dbc81c75aee66bc4d8d0d
Fikayo <fik4christ@yahoo.com>
2019-11-18 00:40:44 +0100

    Edited index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,34 @@
     <div>
       <logo />
       <h1 class="title">
-        circleci-nuxt-app
+        My Nuxt App
       </h1>
       <h2 class="subtitle">
-        Automated Deployment Nuxt App
+        Automated Deployment Nuxt App with Circle CI
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
-        >GitHub</a>
+          >GitHub</a
+        >
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -45,8 +44,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
